8cf77086f611bd97ba28ce40c20c8cd8f19b40ca
Alan Reyes <alanreyes6747@gmail.com>
2024-08-06 14:25:49 -0500

    Display keypresses

--- c-text-editor.c
+++ c-text-editor.c
@@ -1,6 +1,8 @@
 #include <termios.h>
 #include <unistd.h>
-#include <stdlib.h>  
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 
 struct termios original_termios;
@@ -29,6 +31,16 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+    if (iscntrl(c)) {
+        printf("%d\n", c);
+    } else {
+        printf("%d ('%c')\n", c, c);
+    }
+
+
+
+  }
   return 0;
 }
