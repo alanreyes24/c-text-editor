bd6a8e2807be39f55c32017f52801e9fbcb192a5
Alan Reyes <alanreyes6747@gmail.com>
2024-08-06 16:53:54 -0500

    Refactor keyboard input

--- c-text-editor.c
+++ c-text-editor.c
@@ -1,3 +1,5 @@
+/* includes */
+
 #include <termios.h>
 #include <errno.h>
 #include <unistd.h>
@@ -5,16 +7,20 @@
 #include <ctype.h>
 #include <stdio.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/* data */
 
 struct termios original_termios;
 
-void die (const char *s) {
+/* terminal */
 
+void die(const char *s) {
     perror(s);
     exit(1);
-
 }
-
 
 void disableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1) {
@@ -23,13 +29,11 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
-
-    if (tcgetattr(STDIN_FILENO, &original_termios)) {
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1) {
         die("tcgetattr");
     }
 
     atexit(disableRawMode);
-
 
     struct termios changed_termios = original_termios;
 
@@ -37,40 +41,58 @@ void enableRawMode() {
     changed_termios.c_oflag &= ~(OPOST);
     changed_termios.c_cflag |= (CS8);
     changed_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);   
-    changed_termios.c_cc[VMIN] = 0;
-    changed_termios.c_cc[VTIME] = 1; 
+    // changed_termios.c_cc[VMIN] = 0;
+    // changed_termios.c_cc[VTIME] = 1; 
 
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed_termios)) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed_termios) == -1) {
         die("tcsetattr");
     }
 }
 
+void charToBinaryString(char c, char *buffer) {
+    for (int i = 7; i >= 0; i--) {
+        buffer[7 - i] = (c & (1 << i)) ? '1' : '0';
+    }
+    buffer[8] = '\0';
+}
+
+char editorReadKey() {
+
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        }
+    }
+    
+    return c;
+
+}
+
+/* input */
+
+void editorProcessKeypress() {
+
+    char c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
+/* init */
+
 int main() {
 
-  enableRawMode();
+    enableRawMode();
 
-  while(1) {
-
-    char c = '\0';
-    
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-        die("read");
+    while (1) {
+        editorProcessKeypress();
     }
 
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
-
-
-
-  }
-  
-
-
-  return 0;
-
-  }
-
+    return 0;
+}
