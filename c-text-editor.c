c9e07cca51a3b4cedbdb53e17e6859bc8e16d170
Alan Reyes <alanreyes6747@gmail.com>
2024-08-06 15:33:10 -0500

    Add timeout for read()

--- c-text-editor.c
+++ c-text-editor.c
@@ -12,8 +12,6 @@ void disableRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
 }
 
-
-
 void enableRawMode() {
 
     tcgetattr(STDIN_FILENO, &original_termios);
@@ -25,22 +23,33 @@ void enableRawMode() {
     changed_termios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     changed_termios.c_oflag &= ~(OPOST);
     changed_termios.c_cflag |= (CS8);
-    changed_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);    
+    changed_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);   
+    changed_termios.c_cc[VMIN] = 0;
+    changed_termios.c_cc[VTIME] = 1; 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed_termios);
 
 
   
 }
 int main() {
+
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+  while(1) {
+
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
+
+
+
   }
+  
 
 
   return 0;
