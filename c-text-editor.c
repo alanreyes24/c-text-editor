b5b4ee8e47a4f1b9278fc70c7b449ee08d828b1f
Alan Reyes <alanreyes6747@gmail.com>
2024-08-07 13:33:40 -0500

    Add backup window size detection function

--- c-text-editor.c
+++ c-text-editor.c
@@ -23,16 +23,18 @@ struct editorConfig {
 
 struct editorConfig E;
 
-struct termios original_termios;
-
 /* output */
 
 void editorDrawRows() {
-    int y;
-    for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
     }
+  }
 }
+
 
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -43,18 +45,6 @@ void editorRefreshScreen() {
 }
 
 /* terminal */
-
-int getWindowSize(int *rows, int *cols) {
-    struct winsize ws;
-
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
-    } else {
-        *cols = ws.ws_col;
-        *rows = ws.ws_row;
-        return 0;
-    }
-}
 
 void die(const char *s) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -70,74 +60,87 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
-
     if (tcgetattr(STDIN_FILENO, &E.original_termios) == -1) {
         die("tcgetattr");
     }
 
     atexit(disableRawMode);
 
-    struct termios changed_termios = E.original_termios;
-
-    changed_termios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    changed_termios.c_oflag &= ~(OPOST);
-    changed_termios.c_cflag |= (CS8);
-    changed_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);   
-    // changed_termios.c_cc[VMIN] = 0;
-    // changed_termios.c_cc[VTIME] = 1; 
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed_termios) == -1) {
+    struct termios raw = E.original_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
         die("tcsetattr");
     }
 }
 
 char editorReadKey() {
-
     int nread;
     char c;
-
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1 && errno != EAGAIN) {
-            die("read");
-        }
+        if (nread == -1 && errno != EAGAIN) die("read");
     }
-    
     return c;
+}
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  return 0;
+}
+
+
+
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /* input */
 
 void editorProcessKeypress() {
-
     char c = editorReadKey();
-
-    switch(c) {
+    switch (c) {
         case CTRL_KEY('q'):
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
-        exit(0);
-        break;
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
     }
 }
 
 /* init */
 
 void initEditor() {
-    if (getWindowsSize(&E.screenrows, &E.screencols) == -1) {
-        die("getWindowSize");
-    }
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
-
     enableRawMode();
     initEditor();
-
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
     }
-
     return 0;
 }
